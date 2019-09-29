c372f977400a080d4bf8961e42670ab095131dfe
arthev <arthur@evensen.space>
2019-09-30 00:34:24 +0200

    Add skeleton slot-access for prevalence metaclass

--- src/packages.lisp
+++ src/packages.lisp
@@ -2,5 +2,3 @@
   (:use :cl
         :cl-arthur)
   (:export :main))
-
-(use-package :cl-arthur)
--- src/prevalence-utils.lisp
+++ src/prevalence-utils.lisp
@@ -1,5 +1,41 @@
 (in :spiclum)
 
-(defmacro defpclass (class-name superclasses slot-specifiers
-                     &rest class-options)
-  ()) ; TODO
+(defclass prevalence-class (standard-class)
+  ())
+
+(defmethod closer-mop:slot-value-using-class ((class prevalence-class)
+                                   instance
+                                   slot-name)
+  (format t "Accessing: ~S~%~S~%~S~%" class instance slot-name)
+  (call-next-method))
+
+(defmethod (setf closer-mop:slot-value-using-class) (new-value
+                                                     (class prevalence-class)
+                                                     instance
+                                                     slot-name)
+  "Responsible for validation, calling the standard-class setf,
+   and for making and commiting a transaction."
+  (cond ((eq 'standard-class
+             (class-name (class-of (class-of new-value))))
+         (error "Can't handle ~S since it's often STANDARD-CLASS." new-value))
+
+        )
+  (format t "Setting: ~S~%~S~%~S~%~S~%" new-value class instance slot-name)
+  ;; CLHS specifies that setf'ing can return multiple values
+  (let ((results (multiple-value-list (call-next-method))))
+    ;; TODO: Make and commit transaction
+    (values-list results)))
+
+
+
+(defmethod closer-mop:validate-superclass ((class prevalence-class)
+                                           (superclass standard-class))
+  "Just boilerplate declaring metaclass compatibility."
+  t)
+
+
+(defclass test-class ()
+  ((a
+    :initarg :a
+    :accessor a))
+  (:metaclass prevalence-class))
