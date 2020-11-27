982140ed6664369392a8b087bc3c9fa2a467aa57
arthev <arthur@evensen.space>
2020-11-27 15:07:24 +0100

    Save class definitions when saving the world

--- src/interface.lisp
+++ src/interface.lisp
@@ -5,15 +5,27 @@
   (when (find :metaclass class-options :key #'car)
     (error "DEFPCLASS uses implicit metaclass PREVALENCE-CLASS,~%~
             but an explicit metaclass was provided for ~S" class-name))
+  ;; NOTE: It'd also be possible to inject PREVALENCE-OBJECT at the ensure-class-using-metaclass level
   `(defclass ,class-name (,@superclasses prevalence-object)
      ,slot-specifiers
      ,@class-options
      (:metaclass prevalence-class)))
 
 (defun save-world ()
+  "Save a new file as per *PREVALENCE-SYSTEM*'s STORAGE-PATH with time-of-call.
+
+Writes the set of class definitions for prevalence-classes,
+and the set of instances of prevalence-object.".
   (update-prevalence-system-for-timestamp *prevalence-system*
                                           (timestamp-for-new-world))
   (let ((*saving-world-p* t))
+    (maphash
+     (lambda (name last-definition)
+       (declare (ignore name))
+       (destructuring-bind (metaclass class name &rest args) last-definition
+         (declare (ignore metaclass))
+         (serialize-ensure-class-using-metaclass class name args)))
+     (class-definition-store *prevalence-system*))
     (dolist (object (find-all (find-class 'prevalence-object)))
       (serialize-make-instance object))))
 
