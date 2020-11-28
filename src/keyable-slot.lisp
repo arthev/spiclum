27c45569470d17bd6ca6e05d4e752db142ebd37b
arthev <arthur@evensen.space>
2020-11-28 23:09:49 +0100

    Remove unused fns

--- src/keyable-slot.lisp
+++ src/keyable-slot.lisp
@@ -35,33 +35,3 @@
   "Sanity checks KEY."
   (check-type key keyable-slot-key)
   (assert (not (and (eq :class allocation) key))))
-
-;;;; 1. Convenience access
-
-(defun class-x-key-slots (instance-or-class key-type)
-  "Returns a list of slot objects with KEY-TYPE keys.
-
-CONSIDER: Should classes be finalized if not finalized?"
-  (let ((class (if (c2mop:classp instance-or-class)
-                   instance-or-class
-                   (class-of instance-or-class))))
-    (remove-if-not (lambda (slot)
-                     (and (typep slot 'keyable-slot)
-                          (eq (key slot) key-type)))
-                   (c2mop:class-slots class))))
-
-(defun class-unique-key-slots (instance-or-class)
-  "Returns a list of slot objects with :class-unique keys."
-  (class-x-key-slots instance-or-class :class-unique))
-
-(defun index-key-slots (instance-or-class)
-  "Returns a list of slot objects with :index keys."
-  (class-x-key-slots instance-or-class :index))
-
-(defun precedence-unique-key-slots (instance-or-class)
-  "Returns a list of slot objects with :precedence-unique keys"
-  (class-x-key-slots instance-or-class :precedence-unique))
-
-(defun nil-key-slots (instance-or-class)
-  "Returns a list of slot objects with nil keys"
-  (class-x-key-slots instance-or-class nil))
