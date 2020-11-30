311a7b16ac75dcdac223c86915d94ec0c1c1231d
arthev <arthur@evensen.space>
2020-11-30 01:42:23 +0100

    Add skeleton/plan for testing serialization

---
+++ src/test/serialization-tests.lisp
@@ -0,0 +1,27 @@
+(in-package :spiclum)
+
+(5am:def-suite :prevalence.unit.serialization :in :prevalence.unit)
+
+(5am:in-suite :prevalence.unit.serialization)
+
+;; list
+
+;; tree
+
+;; multi-dimensional array
+
+;; hash-table
+
+;; instance as lookup
+
+;; slot-makunbound-using-class
+
+;; setf-slot-value-using-class
+
+;; make-instance
+
+;; ensure-class-using-metaclass
+
+;; change-class
+
+;; reinitialize-instance
