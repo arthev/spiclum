4aaf923aa0ccbffbaf3fab7b381ca927f83fb44c
arthev <arthur@evensen.space>
2020-11-30 23:20:45 +0100

    Add CALL-EQUAL util for testing

--- src/test/serialization-tests.lisp
+++ src/test/serialization-tests.lisp
@@ -4,36 +4,66 @@
 
 (5am:in-suite :prevalence.unit.serialization)
 
-(defmacro serialization-tests (obj &optional (comp 'equalp))
+(defmacro data-tests (obj &optional (comp 'equalp))
   `(progn
      (5am:is (not (eq ,obj (serialize-object ,obj))))
      (5am:is (,comp ,obj (eval (serialize-object ,obj))))))
 
+(defun analogue-call-data (original serialized)
+  (if (symbolp original)
+      (or (eq original serialized)
+          (and (listp serialized)
+               (eq 'quote (car serialized))
+               (eq original (eval serialized))))
+      (let ((serialized (eval serialized)))
+        (funcall (typecase original
+                   (prevalence-object #'eq)
+                   (standard-class #'eq)
+                   (string #'string=)
+                   (array #'equalp)
+                   (hash-table #'equalp)
+                   (number #'eql)
+                   (character #'eql)
+                   (random-state #'equalp)
+                   (c2mop:standard-slot-definition #'slot-name-equivalence)
+                   (t (ignore-args (constantly nil))))
+                 original serialized))))
+
+(defun call-equal (original serialized)
+  ;; This is similar to tree-equal, except one-sided.
+  ;; Rely on ORIGINAL's structure rather than demanding same leaves.
+  (if (atom original)
+      (analogue-call-data original serialized)
+      (and (call-equal (car original) (car serialized))
+           (call-equal (cdr original) (cdr serialized)))))
+
+(defmacro call-tests () ())
+
 (5am:test :list-serialization
   (let ((list '(a b c 59 2.4 "hello" #(1 2 3))))
-    (serialization-tests list))
+    (data-tests list))
   (5am:is (eq nil (eval (serialize-object nil)))))
 
 (5am:test :tree-serialization
   (let ((tree '((((((() . ())))) . ())
                 3 . (a b c (59) . (631 631 631 ("hello") . #("oi" "oy"))))))
-    (serialization-tests tree)))
+    (data-tests tree)))
 
 (5am:test :array-serialization
   (let ((array #(1 2 3)))
-    (serialization-tests array)
+    (data-tests array)
     (let ((string (make-array 3 :element-type 'character
                                 :adjustable t
                                 :fill-pointer t)))
       (format string "hehehe")
-      (serialization-tests string)
+      (data-tests string)
       (5am:is (string= string (eval (serialize-object string))))
       (let ((marray (make-array '(2 2))))
         (setf (aref marray 0 0) array
               (aref marray 0 1) string
               (aref marray 1 0) '(a b c "hmm" 3.3)
               (aref marray 1 1) 'sobering)
-        (serialization-tests marray)))))
+        (data-tests marray)))))
 
 (5am:test :hash-table-serialization
   (let ((ht (make-hash-table :test #'equal))
@@ -43,13 +73,13 @@
                 "yes" no)))
     (loop for (key value) on data by #'cddr
           do (setf (gethash key ht) value))
-    (serialization-tests ht)))
+    (data-tests ht)))
 
 (5am:test :instance-as-lookup-serialization
   (let* ((*prevalence-system* (test-prevalence-system))
          (bottom (apply #'make-instance 'bottom
                         *sample-bottom-unoccupied-plist*)))
-    (serialization-tests bottom eq)))
+    (data-tests bottom eq)))
 
 (5am:test :slot-makunbound-using-class-serialization
   (let* ((*prevalence-system* (test-prevalence-system))
@@ -60,11 +90,10 @@
                      bottom
                      (slot-by-name (find-class 'bottom) 'i-bottom))))
     (destructuring-bind (fn-name class object slotd) call
+      (declare (ignore fn-name))
       (let ((serialized
               (key-args (class object slotd) serialize :slot-makunbound-using-class)))
-        (5am:is (not (eq call serialized)))
-        (5am:is (eq fn-name (car serialized)))
-        (5am:is (equal (cdr call) (mapcar #'eval (cdr serialized))))))))
+        (5am:is (call-equal call serialized))))))
 
 (5am:test :setf-slot-value-using-class-serialization
   (let* ((*prevalence-system* (test-prevalence-system))
@@ -76,15 +105,10 @@
                        ,(slot-by-name (class-of bottom) 'nil-top))
                       christmas-pudding)))
     (destructuring-bind (setf (fn-name class instance slotd) new-value) call
-      (let ((serialized
-              (key-args (new-value class instance slotd) serialize :setf-slot-value-using-class)))
-        (5am:is (not (eq call serialized)))
-        (5am:is (eq 'setf (car serialized)))
-        (5am:is (eq fn-name (caadr serialized)))
-        ;; and now we must check each value for eq of some type...
-        ))))
-
-;; setf-slot-value-using-class
+      (declare (ignore setf fn-name))
+      (let ((serialized (key-args (new-value class instance slotd)
+                                  serialize :setf-slot-value-using-class)))
+        (5am:is (call-equal call serialized))))))
 
 ;; make-instance
 
